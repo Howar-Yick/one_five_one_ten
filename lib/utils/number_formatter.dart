8dbe36eea49354ec1007fb8d6326a66d69c86bf7
Howar-Yick <hao.yick@gmail.com>
2025-12-04 16:45:53 +0800

    按资产类型调整快照单位成本小数位

--- lib/utils/number_formatter.dart
+++ lib/utils/number_formatter.dart
@@ -1,8 +1,14 @@
 import 'package:one_five_one_ten/models/asset.dart';
 
 String formatSnapshotUnitCost(double cost, Asset asset) {
-  if (asset.trackingMethod == AssetTrackingMethod.shareBased) {
+  if (asset.subType == AssetSubType.etf) {
     return cost.toStringAsFixed(3);
   }
-  return cost.toStringAsFixed(4);
+
+  if (asset.subType == AssetSubType.mutualFund) {
+    return cost.toStringAsFixed(4);
+  }
+
+  // 其他资产类型维持默认行为（当前为 3 位小数）
+  return cost.toStringAsFixed(3);
 }
